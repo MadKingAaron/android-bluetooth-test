ae1f673d084958ab55a6a6d0e08f88378207108d
Aaron Jimenez <roceaaan@gmail.com>
2020-02-16 15:13:29 -0800

    First Commit to Add All Files for Project

---
+++ BluetoothTest/app/src/main/java/com/example/bluetoothtest/MainActivity.java
@@ -0,0 +1,151 @@
+package com.example.bluetoothtest;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    BluetoothAdapter mBluetoothAdapter;
+
+    //Create BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mBroadcastReciever1 = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            //When discovery finds a device
+
+            if(action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED))
+            {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
+
+                switch (state){
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG, "onRecieve: STATE OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG, "onRecieve: STATE TURNING OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG, "onRecieve: STATE ON");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG, "onRecieve: STATE TURNING ON");
+                        break;
+                }
+            }
+            //if(BluetoothDevice.ACTION_FOUND.equals(action))
+            //{
+                //Get the BluetoothDevice object from the Intent
+                //BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                //Add the name and address to an array adapter to show in a ListView
+                //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            //}
+        }
+    };
+
+    @Override
+    protected void onDestroy()
+    {
+        Log.d(TAG, "onDestroy: called");
+        super.onDestroy();
+        unregisterReceiver(mBroadcastReciever1);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button btnONOFF = (Button) findViewById(R.id.btnONOFF);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        btnONOFF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: enable/disable BT");
+                enableDisableBT();
+            }
+        });
+    }
+
+
+    public void enableDisableBT(){
+        if (mBluetoothAdapter == null)
+        {
+            Log.d(TAG, "enableDiableBT: DEVICE DOES NOT HAVE ONBOARD BLUETOOTH ADAPTER");
+        }
+
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Log.d(TAG, "enableDisableBT: enabling BT");
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReciever1, BTIntent);
+        }
+
+        else
+         { //bluetooth adapter already enabled
+
+             Log.d(TAG, "enableDisableBT: disabling BT");
+
+            mBluetoothAdapter.disable();
+
+            //IntentFilter to catch state change
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReciever1, BTIntent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
